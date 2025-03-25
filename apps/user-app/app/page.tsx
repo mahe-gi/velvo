ff097a3247513b23dc958a909672ee02b16f1c85
ch mahesh <chmahesh.dev@gmail.com>
2025-03-25 22:43:09 +0530

    feat

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,26 @@
+import Image, { type ImageProps } from "next/image";
+
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+    <div  className=" bg-red-300 h-screen">
+      hello
+    </div>
+  );
+}
