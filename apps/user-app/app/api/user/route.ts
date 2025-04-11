a253199a82ee74aef9d0927fb141857e4df55477
mahesh ch <chmahesh.dev@gmail.com>
2025-04-11 20:11:58 +0530

    initialized prisma /db & basic schema

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from "next/server"
+import { PrismaClient } from "@repo/db/client";
 
-console.log("user route")   
+const client = new PrismaClient();
+
 export const GET = async () => {
+    await client.user.create({
+        data: {
+            email: "asd",
+            name: "adsads"
+        }
+    })
     return NextResponse.json({
         message: "hi there"
     })
