3c7d7b3daf11c5bfc4354eb72243c6e003592f4c
mahesh ch <chmahesh.dev@gmail.com>
2025-05-04 18:24:29 +0530

    fix

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import { Providers } from "./providers";
 
 export default function RootLayout({
   children,
@@ -8,9 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Providers>
-        <body>{children}</body>
-      </Providers>
+      <body>{children}</body>
     </html>
   );
 }
